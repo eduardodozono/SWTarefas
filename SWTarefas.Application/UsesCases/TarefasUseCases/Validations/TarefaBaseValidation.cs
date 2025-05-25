d6911f8ed37970e0368e96f04993f31a9fde2f63
eduardodozono <eduardo.dozono@gmail.com>
2025-05-25 14:53:43 -0300

    adicionando o bogus e refatorando os testes

--- SWTarefas.Application/UsesCases/TarefasUseCases/Validations/TarefaBaseValidation.cs
+++ SWTarefas.Application/UsesCases/TarefasUseCases/Validations/TarefaBaseValidation.cs
@@ -7,9 +7,9 @@ namespace SWTarefas.Application.UsesCases.TarefasUseCases.Validations
     {
         public TarefaBaseValidation()
         {
-            RuleFor(r => r.Titulo).NotEmpty().WithMessage("O campo titulo não pode fica vazio.");
-            RuleFor(r => r.Titulo).MaximumLength(100).WithMessage("O campo titulo tem no máximo 100 cacteres.");
-            RuleFor(r => r.Descricao).MaximumLength(400).WithMessage("O campo titulo tem no máximo 400 cacteres.");
+            RuleFor(r => r.Titulo).NotEmpty().WithMessage("O campo título não pode fica vazio.");
+            RuleFor(r => r.Titulo).MaximumLength(100).WithMessage("O campo título tem no máximo 100 cacteres.");
+            RuleFor(r => r.Descricao).MaximumLength(400).WithMessage("O campo descrição tem no máximo 400 cacteres.");
             RuleFor(r => r.Status).NotEmpty().WithMessage("O campo status não pode fica vazio.");
             RuleFor(r => r.Status).Must(s => Enum.IsDefined(typeof(TarefaStatus), s)).WithMessage("Status inválido (1 - Concluída, 2 - Pendente)");
         }
