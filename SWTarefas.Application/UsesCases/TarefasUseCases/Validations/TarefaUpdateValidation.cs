1c3549e72a8eedeed62734adf2a32a7070d0415b
eduardodozono <eduardo.dozono@gmail.com>
2025-05-26 16:39:54 -0300

    Ajustes testes e apis

--- SWTarefas.Application/UsesCases/TarefasUseCases/Validations/TarefaUpdateValidation.cs
+++ SWTarefas.Application/UsesCases/TarefasUseCases/Validations/TarefaUpdateValidation.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using SWTarefas.Application.UsesCases.TarefasUseCases.DTO;
+using SWTarefas.Application.UsesCases.TarefasUseCases.DTO.Enums;
 using SWTarefas.Infrastructure.DataAcess.Interfaces.Tarefas;
 using SWTarefas.Resources.Resources;
 
@@ -15,7 +16,13 @@ namespace SWTarefas.Application.UsesCases.TarefasUseCases.Validations
 
             RuleFor(r => r.TarefaId).NotEmpty().WithMessage(SWTarefasMessagesExceptions.TarefaVazia);
             RuleFor(r => r.TarefaId).MustAsync(ExisteTarefaId).WithMessage(SWTarefasMessagesExceptions.TarefaNaoExiste);
-            RuleFor(r => r.DataConclusaoPrevista).LessThanOrEqualTo(x => x.DataConclusaoRealizada ).WithMessage(SWTarefasMessagesExceptions.DataConclusaoInferiorDataPrevista);
+            RuleFor(r => r.DataConclusaoPrevista).LessThanOrEqualTo(r => r.DataConclusaoRealizada)
+                .When(r => r.DataConclusaoRealizada.HasValue && r.DataConclusaoPrevista.HasValue).WithMessage(SWTarefasMessagesExceptions.DataConclusaoInferiorDataPrevista);
+            RuleFor(r => r.Status).Equal((int)StatusEnum.TarefaStatus.Concluída)
+                .When(x => x.DataConclusaoRealizada.HasValue).WithMessage(SWTarefasMessagesExceptions.StatusConcluidoErroApiDataRealizada);
+            RuleFor(r => r.Status).Equal((int)StatusEnum.TarefaStatus.Pendente)
+                .When(x => !x.DataConclusaoRealizada.HasValue).WithMessage(SWTarefasMessagesExceptions.StatusPendenteErroApiDataRealizadaVazia);
+
             Include(new TarefaBaseValidation());
         }
 
