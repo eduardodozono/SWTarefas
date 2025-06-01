8551920a7154689f0fe0b2ab0377ff1954c774ea
eduardodozono <eduardo.dozono@gmail.com>
2025-06-01 10:59:03 -0300

    novos testes

--- SWTarefas.Tests/TestsMoq/Common/Entities/TarefasBuilder.cs
+++ SWTarefas.Tests/TestsMoq/Common/Entities/TarefasBuilder.cs
@@ -92,5 +92,51 @@ namespace SWTarefas.Tests.TestsMoq.Common.Entities
 
             return tarefaPendente;
         }
+
+        public static Tarefa Build_Tarefa_Pendente_Sem_Id()
+        {
+            var tarefaPendente = new Faker<Tarefa>()
+               .RuleFor(t => t.Titulo, (t) => t.Person.FullName)
+               .RuleFor(t => t.Descricao, (t) => t.Lorem.Paragraph(1))
+               .RuleFor(t => t.DataConclusaoPrevista, new DateOnly(2025, 1, 1))
+               .RuleFor(t => t.Status, (int)TarefaStatus.Pendente);
+
+            return tarefaPendente;
+        }
+
+        public static Tarefa Build_Tarefa_Concluida_Data_Prevista_Superior_Data_Realizada()
+        {
+            var tarefaPendente = new Faker<Tarefa>().RuleFor(t => t.TarefaId, 1)
+               .RuleFor(t => t.Titulo, (t) => t.Person.FullName)
+               .RuleFor(t => t.Descricao, (t) => t.Lorem.Paragraph(1))
+               .RuleFor(t => t.DataConclusaoPrevista, new DateOnly(2025, 1, 2))
+               .RuleFor(t => t.DataConclusaoRealizada, new DateOnly(2025, 1, 1))
+               .RuleFor(t => t.Status, (int)TarefaStatus.Concluída);
+
+            return tarefaPendente;
+        }
+
+        public static Tarefa Build_Tarefa_Data_Realizada_Status_Pendente()
+        {
+            var tarefaPendente = new Faker<Tarefa>().RuleFor(t => t.TarefaId, 1)
+               .RuleFor(t => t.Titulo, (t) => t.Person.FullName)
+               .RuleFor(t => t.Descricao, (t) => t.Lorem.Paragraph(1))
+               .RuleFor(t => t.DataConclusaoPrevista, new DateOnly(2025, 1, 1))
+               .RuleFor(t => t.DataConclusaoRealizada, new DateOnly(2025, 1, 1))
+               .RuleFor(t => t.Status, (int)TarefaStatus.Pendente);
+
+            return tarefaPendente;
+        }
+
+        public static Tarefa Build_Tarefa_Data_Realizada_Vazia_Status_Concluido()
+        {
+            var tarefaPendente = new Faker<Tarefa>().RuleFor(t => t.TarefaId, 1)
+               .RuleFor(t => t.Titulo, (t) => t.Person.FullName)
+               .RuleFor(t => t.Descricao, (t) => t.Lorem.Paragraph(1))
+               .RuleFor(t => t.DataConclusaoPrevista, new DateOnly(2025, 1, 1))
+               .RuleFor(t => t.Status, (int)TarefaStatus.Concluída);
+
+            return tarefaPendente;
+        }
     }
 }
